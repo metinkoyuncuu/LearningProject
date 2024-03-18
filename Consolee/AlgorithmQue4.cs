067b2d2dfd9ad3dc1e4e09142a5e65666f833818
Koyuncu <130415130+koyuncu5362@users.noreply.github.com>
2024-03-18 23:46:14 +0300

    learning...

---
+++ Consolee/AlgorithmQue4.cs
@@ -0,0 +1,14 @@
+﻿using System.Numerics;
+
+namespace Consolee;
+
+public static class AlgorithmQue4
+{
+    public static int Calculator(DateTime birthDate)
+    {
+        DateTime now = DateTime.Now;
+        var diffrence = now - birthDate;
+        int x =diffrence.Days / 365;
+        return x;
+    }
+}
--- Consolee/Program.cs
+++ Consolee/Program.cs
@@ -13,6 +13,8 @@ namespace Consolee
             Console.WriteLine(algorithQue1.SumCalculator("91111111111", "9999998"));
             Console.WriteLine(AlgorithQue2.PowCalculator(3.5,4));
             Console.WriteLine(AlgorithmQue3.Calculator("1", "3", 4)[0]);
+            Console.WriteLine(AlgorithmQue4.Calculator
+                (new DateTime(2012,6,6)));
             Console.ReadLine(); // Programın kapanmaması için bekletme
         }
         protected internal class x
